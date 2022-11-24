ee3e8badf0529f6f176b0dd846337c605669578a
Pedro Pablo Montes <pmontesarroyave@gmail.com>
2022-11-24 10:00:45 -0500

    el texto de la respuesta cambia

--- public/enviar.js
+++ public/enviar.js
@@ -3,7 +3,9 @@ let parrafo = document.getElementById("parrafoInfo")
 function buttonClick(telefno, correo){
     if(telefno || correo){
         parrafo.innerHTML = 'La informacion se envio correctamete'
+        parrafo.style.color = "#05b3a5"
         return
     }
     parrafo.innerHTML = 'Escribe un correo o telefon porfavor'
+    parrafo.style.color = "rgb(240, 52, 52)"
 }
